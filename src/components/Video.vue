e89386547ed8a74af704f2fe916a4ae1c7ec679c
89s52 <25033402+89s52@users.noreply.github.com>
2018-10-17 09:27:34 +0800

    Create Video.vue

---
+++ src/components/Video.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="myHome" :style="{width: '800px', height: '600px'}">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    this.drawLine()
+  },
+  methods: {
+    drawLine () {
+      // 基于准备好的dom，初始化echarts实例
+      let myHome = this.$echarts.init(document.getElementById('myHome'))
+      // 绘制图表
+      myHome.setOption({
+        title: { text: '在Home中使用echarts' },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
